0788feb15fdeabfc11cd7b23dc700c98638877cd
KatarzynaDF <37115622+KatarzynaDF@users.noreply.github.com>
2018-08-20 17:38:55 +0200

    20.1

---
+++ LookingProcessException.java
@@ -0,0 +1,13 @@
+package com.kodilla.hibernate.manytomany.facade;
+
+import java.security.PublicKey;
+
+public class LookingProcessException extends Exception {
+
+    public static String COMPANY_DOES_NOT_EXIST = "Company not found!";
+    public static String EMPLOYEE_DOES_NOT_EXIST= "Employee not found";
+
+    public LookingProcessException (String message){
+        super(message);
+    }
+}
